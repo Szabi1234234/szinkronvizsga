36eb2a393130521356719e6d0cc36ca5f88fa950
Molnár Maxim <scopsy@icloud.com>
2026-05-11 13:35:08 +0200

    Valamennyi C#

---
+++ C#/KD-0319/csapadek_statisztika/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace csapadek_statisztika
+{
+    internal class Program
+    {
+        // 1. eljárás: kiírja az adatok számát
+        static void AdatokSzama(int[] csapadek)
+        {
+            Console.WriteLine("A beolvasott adatok száma: " + csapadek.Length);
+        }
+
+        // 2. eljárás: kiírja az adatokat egy sorban, vesszővel elválasztva
+        static void AdatokKiirasa(int[] csapadek)
+        {
+            string sor = "";
+            for (int i = 0; i < csapadek.Length; i++)
+            {
+                if (i > 0) sor += ", ";
+                sor += csapadek[i];
+            }
+            Console.WriteLine(sor);
+        }
+
+        // 3. függvény: visszaadja az átlagos csapadékmennyiséget
+        static double Atlag(int[] csapadek)
+        {
+            int osszeg = 0;
+            for (int i = 0; i < csapadek.Length; i++)
+            {
+                osszeg += csapadek[i];
+            }
+            return (double)osszeg / csapadek.Length;
+        }
+
+        // 4. függvény: visszaadja a legnagyobb csapadékértéket
+        static int Maximum(int[] csapadek)
+        {
+            int max = csapadek[0];
+            for (int i = 1; i < csapadek.Length; i++)
+            {
+                if (csapadek[i] > max)
+                    max = csapadek[i];
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Csapadék-statisztika");
+
+            // Fájl beolvasása tömbbe
+            string[] sorok = File.ReadAllLines("csapadek.txt");
+            int[] csapadek = new int[sorok.Length];
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                csapadek[i] = int.Parse(sorok[i]);
+            }
+
+            AdatokSzama(csapadek);
+            AdatokKiirasa(csapadek);
+            Console.WriteLine("Az átlagos csapadék-mennyiség: " + Atlag(csapadek));
+            Console.WriteLine("A legnagyobb érték: " + Maximum(csapadek));
+
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ C#/MM-Csapadek/MM-Csapadek/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MM-Csapadek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SZBI PG")]
+[assembly: AssemblyProduct("MM-Csapadek")]
+[assembly: AssemblyCopyright("Copyright © SZBI PG 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c95ba31-6c2c-4676-981d-a58248dda952")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
